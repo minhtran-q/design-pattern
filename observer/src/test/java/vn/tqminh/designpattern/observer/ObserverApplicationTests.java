b2c9201e3131d1f9dbdcf69b7f789e9c9df8e1f0
Tran Quang Minh <minh.tran.1@swisspost.com>
2022-05-08 22:38:07 +0700

    initialize observer pattern

---
+++ observer/src/test/java/vn/tqminh/designpattern/observer/ObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tqminh.designpattern.observer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
